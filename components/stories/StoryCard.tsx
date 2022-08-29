6e1be7437f65873cc18f5bd77f38cf04d3649795
onyegood <goodmomen@gmail.com>
2022-08-29 22:40:40 +0100

    Adjusted story card style

--- components/stories/StoryCard.tsx
+++ components/stories/StoryCard.tsx
@@ -7,8 +7,11 @@ interface IProps {
 }
 const StoryCard: React.FC<IProps> = ({ story: { name, src, profile } }) => {
   return (
-    <div className="relative h-14 w-14 md:h-20 md:w-20 lg:h-56 lg:w-32 cursor-pointer overflow-x p-3
-    transition duration-200 transform ease-in hover:scale-105 hover:animate-pulse">
+    <div
+      className="relative h-14 w-14 md:h-20 md:w-20 lg:h-56 lg:w-32 cursor-pointer overflow-x p-3
+    transition duration-200 transform ease-in hover:scale-105 hover:animate-pulse"
+    >
+      <div className='flex flex-col items-stretch'>
       <Image
         className="absolute opacity-0 lg:opacity-100 rounded-full z-50 top-10"
         src={profile}
@@ -18,8 +21,10 @@ const StoryCard: React.FC<IProps> = ({ story: { name, src, profile } }) => {
         objectFit="cover"
         alt={name}
       />
+      <p className='absolute font-semi z-50 bottom-5 text-white hidden md:inline-flex'>{name}</p>
+      </div>
       <Image
-        className='object-cover filter brightness-75 rounded-full lg:rounded-3xl'
+        className="object-cover filter brightness-75 rounded-full lg:rounded-3xl"
         src={src}
         layout="fill"
         alt={name}
